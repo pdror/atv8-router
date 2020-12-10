acba7135865413d02405a3d3444c8db15b23d27b
Pedro Ramos <pedrohframos@outlook.com>
2020-12-10 02:12:52 -0300

    add axios request

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -4,24 +4,46 @@ import { Link } from 'react-router-dom';
 import ProminentAppBar from './ProminentAppBar';
 import axios from 'axios';
 
-const requestAlunos = () => {
-    axios.get("https://my-json-server.typicode.com/pdror/atv8-router/alunos")
-        .then((res) => console.log(res.data))
-        .catch((err) => console.log(err));
-}
+class Homepage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            alunos: [],
+        }
+        this.fetchData = this.fetchData.bind(this);
+    }
 
-const Homepage = () => (
-    <div className="Homepage">
-        <ProminentAppBar />
-        <div className="container">
-            <h1>Lista de Alunos</h1>
-            <hr />
-            {requestAlunos()}
-            <Link to="/new">
-                <AddButton />
-            </Link>
-        </div>
-    </div>
-);
+    fetchData() {
+        try {
+            axios.get("https://my-json-server.typicode.com/pdror/atv8-router/alunos")
+            .then((response) => {
+                this.setState({ alunos: response.data })
+            })
+        } catch(err) {
+            console.log(err);
+        }
+    }
+
+
+    render() {
+        return (
+            <div className="Homepage">
+                <ProminentAppBar />
+                <div className="container">
+                    <h1>Lista de Alunos</h1>
+                    <hr />
+                    {
+                        this.fetchData()}
+                    <p>{this.state.alunos.map((aluno) => {
+                        return <p>{aluno.nome}</p>
+                    })}</p>
+                    <Link to="/new">
+                        <AddButton />
+                    </Link>
+                </div>
+            </div>
+        );
+    }
+};
 
 export default Homepage;
